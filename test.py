d84e66687237f2471f84d0ad13628283948fab5b
Wayne <xuwei0517@sjtu.edu.cn>
2018-07-12 13:49:30 +0800

    modify state into (3,)

---
+++ test.py
@@ -0,0 +1,36 @@
+import gym
+import sys
+sys.path.append('C:/Users/xuwei1/Documents/baselines')
+from baselines import deepq
+import time
+
+def main():
+    env = gym.make('Catcher-v0')
+    model = deepq.models.mlp([64])
+    act = deepq.learn(
+        env,
+        q_func=model,
+        lr=1e-3,
+        max_timesteps=10000,
+        buffer_size=50000,
+        exploration_fraction=0.1,
+        exploration_final_eps=0.02,
+        print_freq=1
+    )
+    print("Saving model to catcher_model.pkl")
+    act.save("model/catcher.pkl")
+
+def test():
+    env = gym.make('Catcher-v0')
+    state = env.reset()
+    print(state.shape)
+    for _ in range(10):
+    	env.render()
+    	time.sleep(0.5)
+    	state, reward, done, _= env.step(env.action_space.sample()) # take a random action
+    	print((state.shape,reward,done))
+    	if done:
+    		break
+
+if __name__ == '__main__':
+    main()
